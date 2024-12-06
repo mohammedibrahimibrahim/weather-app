469a5defbe8ca79adfdcf7f71d972cf179f2c7d2
mohammedibrahimibrahim <157136446+mohammedibrahimibrahim@users.noreply.github.com>
2024-12-06 10:57:12 +0530

    change 404

--- script1.js
+++ script1.js
@@ -13,7 +13,7 @@ const getWeather = async(city) => {
 }
 
 const showWeather = (data) => {
-    if (data.cod == "") {
+    if (data.cod == "404") {
         weather.innerHTML = `<h2> City Not Found <h2>`
         return;
     }
